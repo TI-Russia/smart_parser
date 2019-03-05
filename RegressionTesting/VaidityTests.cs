1c37344a14f984ebc45d90a8652783c6f7a2c30c
Ilya Korobkov <i.korobkov@eastwind.ru>
2019-03-05 17:18:01 +0500

    Added regression test project

---
+++ RegressionTesting/VaidityTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegressionTesting
+{
+    [TestClass]
+    public class ValidityTests
+    {
+        [TestMethod]
+        public void ExcelTests()
+        {
+        }
+
+        [TestMethod]
+        public void WordTests()
+        {
+
+        }
+    }
+}
