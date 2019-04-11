f06fd6a2de15391fb467bea0ad58c8de0663ed65
Ilya Korobkov <handfulofvoid@gmail.com>
2019-04-11 18:49:42 +0500

    added DeclarationField.cs missing from previous commit

---
+++ ParserCommon/DeclarationField.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TI.Declarator.ParserCommon
+{
+    public enum DeclarationField : short
+    {
+        None = 0,
+        Number,
+        NameOrRelativeType,
+        Occupation,
+
+        // Для случая, когда один и тот же набор колонок содержит сведения и о частной, и о государственной собственности
+        MixedRealEstateType,
+        MixedRealEstateArea,
+        MixedRealEstateCountry,
+        MixedRealEstateOwnershipType,
+
+        OwnedRealEstateType,
+        OwnedRealEstateOwnershipType,
+        OwnedRealEstateArea,
+        OwnedRealEstateCountry,
+
+        StatePropertyType,
+        StatePropertyArea,
+        StatePropertyCountry,
+
+        Vehicle,
+        DeclaredYearlyIncome,
+        DataSources,
+        VehicleType,
+        VehicleModel,
+    }
+}
