4007b519bc71c7fe79653bbb342c44c75cf095b0
U-LD\sokirko <C:\Users\sokirko\AppData\Roaming\The Bat!>
2019-05-10 21:22:58 +0300

    use program arguments

--- Tindalos/Program.cs
+++ Tindalos/Program.cs
@@ -10,38 +10,84 @@ using TI.Declarator.JsonSerialization;
 using TI.Declarator.ParserCommon;
 using TI.Declarator.WordParser;
 
+using CMDLine;
+
 namespace Tindalos
 {
+    
     public class Tindalos
     {
         private static readonly string ErrorsFile = "errors.log";
         private static Dictionary<String, RealEstateType> PropertyTypes = new Dictionary<string, RealEstateType>();
+        private static bool WriteJson = false;
+        private static bool WriteHeader = false;
+        private static string Action = "scan";
+        private static bool Verbose = false;
 
         static Tindalos()
         {
 
         }
+        static string ParseArgs (string[] args)
+        {
+            CMDLineParser parser = new CMDLineParser();
+            CMDLineParser.Option actionOpt = parser.AddStringParameter("--action", "scan, test or full", true);
+            CMDLineParser.Option writeJsonOpt = parser.AddBoolSwitch("--write-json", "write json");
+            CMDLineParser.Option writeHeaderOpt = parser.AddBoolSwitch("--write-header", "write header");
+            CMDLineParser.Option verboseOpt = parser.AddBoolSwitch("--verbose", "verbose");
+            try
+            {
+                //parse the command line
+                parser.Parse(args);
+            }
+            catch (Exception ex)
+            {
+                //show available options      
+                Console.Write(parser.HelpMessage());
+                Console.WriteLine();
+                Console.WriteLine("Error: " + ex.Message);
+                throw;
+            }
+
+            //replace argument list with remaining arguments
+            WriteJson = writeJsonOpt.isMatched;
+            WriteHeader = writeHeaderOpt.isMatched;
+            Verbose = verboseOpt.isMatched;
+            Action = actionOpt.Value.ToString();
+            return String.Join(" ", parser.RemainingArgs()).Trim(new char[] { '"' });
+        }
 
         static void Main(string[] args)
         {
-            //Scan(args);
-            //Test(@"testfiles\2016_Sotrudniki_ministerstva.docx");
-            //Test(@"testfiles\A - min_res_2011_Sotrudniki_ministerstva.doc");
-            Test(@"testfiles\C - min_health_2015_Sotrudniki_ministerstva.docx");
-            //Test(@"to_process\min_ind\2017_Sotrudniki_ministerstva_(utochnionnye).docx");
+            string inputFileName = ParseArgs(args);
+            if (Action == "scan")
+            {
+                Scan(inputFileName);
+            }
+            else if (Action == "test")
+            {
+                Test(inputFileName);
+            }
+            else if (Action == "full") {
+                Process(inputFileName);
+            }
+            else
+            {
+                Console.WriteLine("Error: unknown action" + Action);
+            }
 
-            //var ue = new UnknownEntry
-            //{
-            //    Contents = "квортира",
-            //    EntryType = "realestatetype",
-            //    FileName = "imaginary_file.docx",
-            //    DocumentFileId = "1337",
-            //    WordPageNumber = 3
-            //};
+                //var ue = new UnknownEntry
+                //{
+                //    Contents = "квортира",
+                //    EntryType = "realestatetype",
+                //    FileName = "imaginary_file.docx",
+                //    DocumentFileId = "1337",
+                //    WordPageNumber = 3
+                //};
 
-            //ApiClient.ReportUnknownEntry(ue);
+                //ApiClient.ReportUnknownEntry(ue);
 
-            Console.WriteLine("Press any key..");
+                Console.WriteLine("Press any key..");
             Console.ReadKey();
         }
 
@@ -63,21 +109,19 @@ namespace Tindalos
             return outputFileName;            
         }
 
-        static void Scan(string[] args)
+        static void Scan(string inputFileName)
         {
-            string arg = String.Join(" ", args).Trim(new char[] { '"' });
-            if (Directory.Exists(arg))
+            if (Directory.Exists(inputFileName))
             {
-                ScanDir(arg);
+                ScanDir(inputFileName);
             }
-            else if (File.Exists(arg))
+            else if (File.Exists(inputFileName))
             {
-                string outputFile = Path.GetFileNameWithoutExtension(arg) + ".txt";
-                ScanFile(arg, outputFile);
+                ScanFile(inputFileName);
             }
             else
             {
-                Console.WriteLine($"File or directory {arg} not found.");
+                Console.WriteLine($"File or directory {inputFileName} not found.");
             }                     
 
             Console.WriteLine("Finished. Press any key.");
@@ -89,7 +133,8 @@ namespace Tindalos
             string ext = Path.GetExtension(fileName);
             switch (ext)
             {
-                case ".doc": string docXName = Doc2DocX(fileName); return ParseDocX(docXName);
+                case ".html":
+                case ".doc": return ParseDocX(Doc2DocX(fileName));
                 case ".docx": return ParseDocX(fileName);
                 default: throw new Exception(@"Unsupported format in file {fileName}");
             }
@@ -111,7 +156,14 @@ namespace Tindalos
         private static Declaration ParseDocX(string fileName)
         {
             var parser = new DocXParser();
-            return parser.Parse(fileName);
+            Declaration res = parser.Parse(fileName, Verbose);
+            if (WriteJson)
+            {
+                string outputFile = Path.GetFileNameWithoutExtension(fileName) + ".json";
+                File.WriteAllText(outputFile, DeclarationSerializer.Serialize(res));
+            }
+
+            return res;
         }
 
         private static void ScanDir(string dir)
@@ -122,7 +174,7 @@ namespace Tindalos
             string outputFile = dirName + ".txt";
             foreach (string fileName in Directory.GetFiles(dir))
             {
-                ScanFile(fileName, outputFile);
+                ScanFile(fileName);
             }
 
             foreach (string subdir in Directory.GetDirectories(dir))
@@ -131,7 +183,7 @@ namespace Tindalos
             }
         }
 
-        private static void ScanFile(string fileName, string outputFile = null)
+        private static void ScanFile(string fileName)
         {
             Console.WriteLine($"Scanning file {fileName}");
 
@@ -140,8 +192,9 @@ namespace Tindalos
                 string ext = Path.GetExtension(fileName);
                 switch (ext)
                 {
-                    case ".doc": string docXName = Doc2DocX(fileName); ScanDocX(docXName, outputFile); break;
-                    case ".docx": ScanDocX(fileName, outputFile); break;
+                    case ".html":
+                    case ".doc":  ScanDocX(Doc2DocX(fileName)); break;
+                    case ".docx": ScanDocX(fileName); break;
                     default: break;
                 }
             }
@@ -152,13 +205,14 @@ namespace Tindalos
             }
         }
 
-        private static void ScanDocX(string fileName, string outputFile = null)
+        private static void ScanDocX(string fileName)
         {
             var parser = new DocXParser();
             var co = parser.ScanProperties(fileName).ColumnOrdering;
 
-            if (outputFile != null)
+            if (WriteHeader)
             {
+                string outputFile = Path.GetFileNameWithoutExtension(fileName) + ".txt";
                 var outBuilder = new StringBuilder();
                 outBuilder.AppendLine(fileName);
                 foreach (var fieldObj in Enum.GetValues(typeof(DeclarationField)))
