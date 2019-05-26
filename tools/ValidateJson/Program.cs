fada60a1c29c46a4f810a5c4a104874859e1be64
U-LD\sokirko <C:\Users\sokirko\AppData\Roaming\The Bat!>
2019-05-26 17:49:27 +0300

    add new tool to validate json from command line

---
+++ tools/ValidateJson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using TI.Declarator.DeclaratorApiClient;
+
+namespace ValidateJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileContents = File.ReadAllText(args[0]);
+            Console.WriteLine(ApiClient.ValidateParserOutput(fileContents));
+        }
+    }
+}
