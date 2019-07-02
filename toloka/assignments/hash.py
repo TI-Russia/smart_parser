665909ba4601fb0c70e904fbb1fae01673e48289
U-LD\sokirko <sokirko@yandex.ru>
2019-07-02 17:23:02 +0300

    add hash codes

---
+++ toloka/assignments/hash.py
@@ -0,0 +1,34 @@
+﻿import re
+import sys
+
+# normal usage: 
+# python hash.py golden_1.tsv >golden_1.tsv.hashed_json
+
+def hashCodeNoSpaces(s):
+    h = 0
+    s = re.sub(r"[\s():;,\".-]", "", s)
+    for c in s:
+        h = (31 * h + ord(c)) & 0xFFFFFFFF
+    return ((h + 0x80000000) & 0xFFFFFFFF) - 0x80000000;
+
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+lineCount = 0
+for x in  open(sys.argv[1], "r", encoding="utf8"):
+    items = list(x.split("\t"))
+    if len(items) <= 1:
+        continue
+    if len(items) != 4:
+        sys.stderr.write("cannot parse input, line = " + str(lineCount+1));
+        exit(1)
+        
+    if lineCount == 0:
+       hashcode = "GOLDEN:declaration_hashcode";
+    else:
+       hashcode = str(hashCodeNoSpaces(items[2]))
+    items[2] = hashcode; 
+    line = "\t".join(items)
+    sys.stdout.write(line)        
+    lineCount += 1
+
