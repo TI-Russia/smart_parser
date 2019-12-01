77aee6db462bdc53ae0bb31a08b49e67c4e4a24f
U-LD\sokirko <sokirko@yandex.ru>
2019-12-01 03:43:44 +0300

    update section language model

--- RegressionTesting/ValidityTests.cs
+++ RegressionTesting/ValidityTests.cs
@@ -493,6 +493,13 @@ namespace RegressionTesting
             TestSmartParser("SmartParser\\MinTrans2009.doc", "prod");
         }
 
+        [TestMethod]
+        [TestCategory("doc")]
+        public void MinKult2015Doc()
+        {
+            TestSmartParser("SmartParser\\MinKult2015.doc", "prod");
+        }
+
         private static void SetupLog4Net()
         {
             log4net.Config.XmlConfigurator.Configure(new FileInfo("log4net.config"));
--- lib/Adapters/Section.cs
+++ lib/Adapters/Section.cs
@@ -15,20 +15,33 @@ namespace Smart.Parser.Adapters
             {
                 return true;
             }
-            if (cellText.StartsWith("Федеральн") ||
-                cellText.StartsWith("ФКУ") ||
-                cellText.StartsWith("ФГУП") ||
+            // first words: get it from previous results:
+            // ~/media/json$ ls | xargs  jq -cr '.persons[].person.department' | awk '{print $1}' | sort | uniq -c  | sort -nr
+            if (cellText.StartsWith("ФК") ||
+                cellText.StartsWith("ФГ") ||
+                cellText.StartsWith("ГУ") ||
+                cellText.StartsWith("Федеральн") ||
                 cellText.StartsWith("ФБУ") ||
                 cellText.StartsWith("Руководство") ||
                 cellText.StartsWith("ФАУ") ||
                 cellText.StartsWith("Департамент") ||
                 cellText.StartsWith("Заместители") ||
                 cellText.StartsWith("Институт") ||
-                cellText.StartsWith("ФГБУ") ||
                 cellText.StartsWith("Государственное") ||
+                cellText.StartsWith("Главное") ||
+                cellText.StartsWith("Отдел") ||
+                cellText.StartsWith("Управление") ||
                 cellText.StartsWith("Фонд") ||
-                cellText.StartsWith("АНО")
+                cellText.StartsWith("АНО") ||
+                cellText.StartsWith("УФСИН") ||
+                cellText.StartsWith("Центр") ||
+                cellText.StartsWith("ФСИН") ||
+                cellText.StartsWith("Министерство") ||
+                cellText.StartsWith("Лица") ||
+                cellText.StartsWith("ИК")
                 )
+
+                
             {
                 return true;
             }
