b3b3ad6816ed59d16ac9e17d412fe82992765db6
U-LD\sokirko <sokirko@yandex.ru>
2020-01-19 11:19:29 +0300

    time info fot tests

--- tools/robots/dlrobot/run_test.sh
+++ tools/robots/dlrobot/run_test.sh
@@ -2,6 +2,8 @@
 #ls tests/*.txt | xargs --verbose -n 1 -P 4  python dlrobot.py --logfile temp  --rebuild  --start-from last_step  --project 
 #--skip-final-download
 
+date
+
 ls tests/*.txt | xargs --verbose -I '{}' -n 1 -P 5  \
    sh -c "python dlrobot.py --logfile temp  --rebuild  --project \"\$1\" "  -- {} 
 
@@ -11,3 +13,5 @@ if [ $? -eq 0 ]; then
 else
     echo  "==== TESTS FAILED ====="!
 fi
+
+date
