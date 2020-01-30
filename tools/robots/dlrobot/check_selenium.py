073a76874328befc8a52c2194076323dc25b2b0f
U-LD\sokirko <sokirko@yandex.ru>
2020-01-30 20:21:22 +0600

    simple selenium test

--- tools/robots/dlrobot/check_selenium.py
+++ tools/robots/dlrobot/check_selenium.py
@@ -20,6 +20,7 @@ def setup_logging( logger, logfilename):
     logger.addHandler(fh)
 
 def click_element(driver, element):
+    logger = logging.getLogger("my_logger")
     print("click element with text {}".format(element.text.strip('\n\r\t ')))
     #driver.execute_script('window.scrollTo(0,{});'.format(element.location['y']))
     driver.execute_script("arguments[0].scrollIntoView({block: \"center\", behavior: \"smooth\"});", element)
@@ -36,7 +37,7 @@ def click_element(driver, element):
 
     time.sleep(6)
     if len(driver.window_handles) < 2:
-        step_info.website.logger.debug("cannot click, no new window is found")
+        logger.debug("cannot click, no new window is found")
         return
     window_after = driver.window_handles[1]
     driver.switch_to.window(window_after)
@@ -46,7 +47,7 @@ logger = logging.getLogger("my_logger")
 setup_logging(logger, "check_selenium.log")
 
 options = FirefoxOptions()
-#options.headless = True
+options.headless = True
 driver = webdriver.Firefox(options=options)
 #driver.get("http://www.ya.ru")
 #driver.get("http://oblsud.tula.sudrf.ru") # windows-1251
@@ -70,5 +71,5 @@ click_element(driver, elements[368])
 
 print ("url after click")
 print (driver.current_url)
-
+assert driver.current_url == "http://www.mkrf.ru/about/territorial_authorities/upravlenie_ministerstva_kultury_rossiyskoy_federatsii_po_tsentralnomu_federalnomu_okrugu_/anti-corruption/"
 driver.quit()
