bd4fc8cfaec3e41742a74e8c96a754aac5b25724
Alexey Sokirko <sokirko@yandex.ru>
2020-03-12 18:31:26 +0300

    add synthetic tests

---
+++ tools/robots/dlrobot/tests/run_tests.sh
@@ -0,0 +1,13 @@
+for test_folder in simple; do
+  echo -n "test $test_folder -> "
+  cd $test_folder
+  [ ! -f test_log.out ] || rm test_log.out
+  bash -x run.sh >test_log.out 2>&1
+  if [ $? -eq 0 ]; then
+     echo "success"
+  else
+     echo "failed"
+     exit 1
+  fi
+  cd - >/dev/null
+done
---
+++ tools/robots/dlrobot/tests/simple/run.sh
@@ -0,0 +1,14 @@
+python -m http.server --bind 127.0.0.1 --directory html 8090 &
+web_server_pid=$!
+disown
+
+python ../../dlrobot.py --project project.txt 
+
+kill $web_server_pid >/dev/null
+
+files_count=`find result -type f | wc -l`
+if [ $files_count != 1]; then
+    echo "no exported file found"
+    exit 1
+fi
+
