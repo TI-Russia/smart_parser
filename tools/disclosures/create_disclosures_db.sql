7bc3160d240d0753431b1f9d294213d84f9b65e5
Alexey Sokirko <sokirko@yandex.ru>
2020-04-14 13:03:13 +0300

    delete empty lines

--- tools/disclosures/create_disclosures_db.sql
+++ tools/disclosures/create_disclosures_db.sql
@@ -1,10 +1,5 @@
 drop database if exists disclosures_db;
-
 create database disclosures_db character set utf8mb4 collate utf8mb4_unicode_ci;
-
 create user if not exists 'disclosures'@ identified by 'disclosures';
-
-grant all privileges on `disclosures_db`:.* to 'disclosures'@;
-
-use disclosures_db;
+grant all privileges on `disclosures_db`.* to 'disclosures'@;
 
