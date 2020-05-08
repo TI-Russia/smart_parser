4a0f1010dd9fb74797b1661c2c4ea05fb50c470b
Alexander Serechenko <skabbit@gmail.com>
2020-05-08 13:59:53 +1000

    FontWidthTest introduced, generates test JPEG to analyse diffs

--- test/DocAdapterTest.cs
+++ test/DocAdapterTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Smart.Parser.Adapters;
 using System.IO;
@@ -12,22 +14,48 @@ namespace test
         [TestMethod]
         public void AsposeDocAdapterTest()
         {
-            string docFile = Path.Combine(TestUtil.GetTestDataPath(), "E - min_sport_2012_Rukovoditeli_gospredpriyatij,_podvedomstvennyih_ministerstvu.doc");
+            var fileName = "E - min_sport_2012_Rukovoditeli_gospredpriyatij,_podvedomstvennyih_ministerstvu.doc";
+            string docFile = Path.Combine(TestUtil.GetTestDataPath(), fileName);
             IAdapter adapter = AsposeDocAdapter.CreateAdapter(docFile);
 
         }
-        
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for(j = 0; j < encoders.Length; ++j)
+            {
+                if(encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }            
+
         [TestMethod]
         public void FontWidthTest()
         {
-            var graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1));
+            var myBitmap = new Bitmap(250, 20);
+            var graphics = System.Drawing.Graphics.FromImage(myBitmap);
             var FontName = "Times New Roman";
             var testLine = "Test width of long string - whats that?";
-            var FontSize = 10;
+            var FontSize = 20;
             var font = new System.Drawing.Font(FontName, FontSize / 2);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            graphics.DrawString(testLine, font, drawBrush, 0, 0);
+            
+            var myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            var myEncoder = Encoder.Quality;
+            var myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+            var myEncoderParameters = new EncoderParameters(1);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            myBitmap.Save("test-string.jpg", myImageCodecInfo, myEncoderParameters);
+            
             var stringSize = graphics.MeasureString(testLine, font);
-            Assert.AreEqual(103, stringSize.Width);
-            Assert.AreEqual(8, stringSize.Height);
+            Assert.AreEqual(103 * 2, stringSize.Width);
+            Assert.AreEqual(15, stringSize.Height);
         }
         
     }
