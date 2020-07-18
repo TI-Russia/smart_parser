731d80320615beec90c614787f15feae86fe1bf2
sokirko <sokiko@yandex.ru>
2020-07-18 17:27:43 +0300

    bug fix

--- tools/disclosures/declarations/management/commands/select_max_person_name.py
+++ tools/disclosures/declarations/management/commands/select_max_person_name.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
         for p in models.Person.objects.all():
             updated = False
             if p.declarator_person_id is None:
-                for s in p.section_set():
+                for s in p.section_set.all():
                     if len(p.person_name) < len(s.person_name):
                         p.person_name = s.person_name
                         print ("set {} to person.id={}".format(p.person_name, p.id))
