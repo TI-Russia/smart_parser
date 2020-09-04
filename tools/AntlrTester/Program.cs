1adfed96f56c4417623c34e3725b309335135d5a
Alvin J. Alexander <sokirko@yandex.ru>
2020-09-04 18:19:58 +0300

    use program args

---
+++ tools/AntlrTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AntlrTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args[0];
+            var output = input + ".result";
+            var texts = AntlrTestUtilities.ReadTestCases(input);
+            AntlrTestUtilities.ProcessTestCases(texts, output);
+        }
+    }
+}
