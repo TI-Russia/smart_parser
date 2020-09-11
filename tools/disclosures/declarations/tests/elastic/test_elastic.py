e41b95c6800100de07558a53c29c9d12bfcf0253
sokirko74 <sokirko@yandex.ru>
2020-09-11 15:51:51 +0300

    fix disclosures tests

--- tools/disclosures/declarations/tests/elastic/test_elastic.py
+++ tools/disclosures/declarations/tests/elastic/test_elastic.py
@@ -15,8 +15,19 @@ class ElasticTestCase(TestCase):
         self.assertEqual(len(people), 0)
         people = list(ElasticSectionDocument.search().query('match', person_name='Иванов'))
         models.Section.objects.all().delete()
+        models.Source_Document.objects.all().delete()
+
+        ofc = models.Office()
+        ofc.name = "some name"
+        ofc.save()
+
+        src_doc = models.Source_Document()
+        src_doc.office  = ofc
+        src_doc.save()
+
         section = models.Section()
         section.person_name = "Иванов Иван"
+        section.source_document = src_doc
         section.save()
         print("sleep 2 seconds till elastic processes records")
         time.sleep(2)
