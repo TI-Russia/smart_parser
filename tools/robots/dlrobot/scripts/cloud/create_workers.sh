4b7f58b487ba7d257e7240fb17222e40e314e37b
Alvin J. Alexander <sokirko@yandex.ru>
2020-09-29 16:30:12 +0300

    try run dlrobot worker as a service

--- tools/robots/dlrobot/scripts/cloud/create_workers.sh
+++ tools/robots/dlrobot/scripts/cloud/create_workers.sh
@@ -1,10 +1,17 @@
+HOME_DIR=/home/sokirko
+export ASPOSE_LIC=$HOME_DIR/lic.bin
+export PYTHONPATH=$HOME_DIR/smart_parser/tools
+export DECLARATOR_CONV_URL=disclosures.ru:8091
+export DLROBOT_CENTRAL_URL=disclosures.ru:8089
+
+
 function create_worker() {
   local path=$1
   if [ ! -d $path ]; then
     mkdir -p $path
     cd $path
-    rm nohup.out; nohup python3 ~/smart_parser/tools/robots/dlrobot/scripts/cloud/dlrobot_worker.py \
-              --server-address disclosures.ru:8089  --tmp-folder /tmp --run-forever &
+    rm nohup.out; nohup python3 $HOME_DIR/smart_parser/tools/robots/dlrobot/scripts/cloud/dlrobot_worker.py \
+              --server-address $DLROBOT_CENTRAL_URL  --tmp-folder /tmp --run-forever &
     cd -
   fi
 }
