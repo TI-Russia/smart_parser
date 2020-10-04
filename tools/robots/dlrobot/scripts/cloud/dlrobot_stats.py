acddba3495fba10b4e3af6da6d100257bebd4dce
sokirko74 <sokirko@yandex.ru>
2020-10-04 06:35:07 +0300

    dlrobot monitoring dashboard

--- tools/robots/dlrobot/scripts/cloud/dlrobot_stats.py
+++ tools/robots/dlrobot/scripts/cloud/dlrobot_stats.py
@@ -13,22 +13,28 @@ def parse_args():
                         help="for example /home/sokirko/declarator_hdd/declarator/2020-09-29/processed_projects/dlrobot_remote_calls.dat")
     parser.add_argument("--conversion-server-stats", dest='conversion_server_stats', required=False,
                         help="for example /home/sokirko/declarator_hdd/declarator/convert_stats.txt")
+    parser.add_argument("--output-folder", dest='output_folder', required=False, default=".",
+                        help="for example ~/smart_parser.disclosures_prod/tools/disclosures/disclosures/static")
     args = parser.parse_args()
     return args
 
 
-def build_html(fig, output_file):
+def build_html(args, fig, output_file):
+    output_file = os.path.join(args.output_folder, output_file)
     fig.write_html(output_file, include_plotlyjs='cdn')
 
 
 class TCumulativeStats:
-    def __init__(self, central_stats_file, min_date=None):
-        self.remote_calls = TRemoteDlrobotCall.read_remote_calls_from_file(central_stats_file)
+    def __init__(self,  args, min_date=None):
+        self.args = args
+        self.min_date = min_date
+        self.remote_calls = TRemoteDlrobotCall.read_remote_calls_from_file(args.central_stats_file)
         self.cumulative_declaration_files_count = []
         self.cumulative_processed_websites_count = []
         self.end_times = []
         self.websites = []
-        self.worker_host_names = []
+        self.projects_by_workers = []
+        self.declaration_files_by_workers = []
         self.build_stats(min_date)
 
     def build_stats(self, min_date=None):
@@ -41,7 +47,8 @@ class TCumulativeStats:
             dttime = datetime.datetime.fromtimestamp(remote_call.end_time)
             self.end_times.append(pd.Timestamp(dttime))
             self.websites.append(remote_call.get_website())
-            self.worker_host_names.append(remote_call.worker_host_name)
+            self.projects_by_workers.append(remote_call.worker_host_name)
+            self.declaration_files_by_workers.extend([remote_call.worker_host_name] * remote_call.result_files_count)
 
             sum_count += remote_call.result_files_count
             self.cumulative_declaration_files_count.append(sum_count)
@@ -53,42 +60,52 @@ class TCumulativeStats:
         df = pd.DataFrame({'Date': self.end_times,
                            "DeclarationFileCount": self.cumulative_declaration_files_count,
                            "website": self.websites})
+        title = 'Declaration Crawling Progress'
+        if self.min_date is not None:
+            title += " start from " + str(self.min_date)
         fig = px.line(df, x='Date', y='DeclarationFileCount',
                       hover_data=["website"],
-                      title='Declaration Crawling Progress')
-        build_html(fig, html_file)
+                      title=title)
+        build_html(self.args, fig, html_file)
 
     def write_website_progress(self, html_file):
         df = pd.DataFrame({
              'Date': self.end_times,
              "WebSiteCount": self.cumulative_processed_websites_count,
              "website": self.websites})
-        fig = px.line(df, x='Date', y='WebSiteCount', title='Web Site Progress', hover_data=["website"])
-        build_html(fig, html_file)
+        title = 'Web Site Progress'
+        if self.min_date is not None:
+            title += " start from " + str(self.min_date)
+        fig = px.line(df, x='Date', y='WebSiteCount', title=title, hover_data=["website"])
+        build_html(self.args, fig, html_file)
 
 
-def process_cumulative_stats(central_stats_file):
-    stats = TCumulativeStats(central_stats_file)
+def process_cumulative_stats(args):
+    stats = TCumulativeStats(args)
     stats.write_declaration_crawling_stats('declaration_crawling_stats.html')
-    stats.write_website_progress('file_progress.html')
+    stats.write_website_progress( 'file_progress.html')
 
     min_time = datetime.datetime.now() - datetime.timedelta(hours=12)
-    stats = TCumulativeStats(central_stats_file, min_time)
+    stats = TCumulativeStats(args, min_time)
     stats.write_declaration_crawling_stats('declaration_crawling_stats_12h.html')
     stats.write_website_progress('file_progress_12h.html')
 
+    df = pd.DataFrame({'projects_by_workers': stats.projects_by_workers})
+    fig = px.histogram(df, x="projects_by_workers", title="Projects By Workers (12 hours)")
+    build_html(args, fig, "worker_stats_12h.html")
 
-    df = pd.DataFrame({'worker_host_names': stats.worker_host_names})
-    fig = px.histogram(df, x="worker_host_names")
-    build_html(fig, "worker_stats.html")
+    df = pd.DataFrame({'declaration_files_by_workers': stats.declaration_files_by_workers})
+    fig = px.histogram(df, x="declaration_files_by_workers", title="Declaration Files By Workers (12 hours)")
+    build_html(args, fig, "declaration_files_by_workers_12h.html")
 
 
 class TPointStats:
-    def __init__(self, central_stats_file):
-        self.remote_calls = TRemoteDlrobotCall.read_remote_calls_from_file(central_stats_file)
+    def __init__(self, args):
+        self.args = args
+        self.remote_calls = TRemoteDlrobotCall.read_remote_calls_from_file(args.central_stats_file)
         self.minutes = []
         self.websites = []
-        self.worker_host_names = []
+        self.projects_by_workers = []
         self.exported_files_counts = []
         self.end_time_stamps = []
         self.build_stats()
@@ -101,32 +118,32 @@ class TPointStats:
                 continue
             self.minutes.append(remote_call.get_total_minutes())
             self.websites.append(remote_call.get_website())
-            self.worker_host_names.append(remote_call.worker_host_name)
+            self.projects_by_workers.append(remote_call.worker_host_name)
             self.exported_files_counts.append(remote_call.result_files_count)
             end_time = datetime.datetime.fromtimestamp(remote_call.end_time)
             self.end_time_stamps.append(end_time.strftime("%Y-%m-%d %H:%M:%S"))
 
-    def write_stats(self, html_file):
+    def write_stats(self,  html_file):
         df = pd.DataFrame({
             'Minutes': self.minutes,
             "Websites": self.websites,
-            "hostnames": self.worker_host_names,
+            "hostnames": self.projects_by_workers,
             "exported_files_counts": self.exported_files_counts,
             'end_times': self.end_time_stamps})
 
         fig = px.line(df, x='end_times', y='Minutes',
                       hover_data=['Websites', "hostnames", "exported_files_counts", "end_times"],
                       title='Crawl Website Speed')
-        build_html(fig, html_file)
+        build_html(self.args, fig, html_file)
 
 
-def process_point_stats(central_stats_file):
-    stats = TPointStats(central_stats_file)
+def process_point_stats(args):
+    stats = TPointStats(args)
     stats.write_stats('web_site_speed.html')
 
 
-def process_convert_stats(history_filename):
-    with open(history_filename, encoding="utf8") as inp:
+def process_convert_stats(args):
+    with open(args.conversion_server_stats, encoding="utf8") as inp:
         timestamps = list()
         ocr_pending_all_file_sizes = list()
         for l in inp:
@@ -138,16 +155,15 @@ def process_convert_stats(history_filename):
     df = pd.DataFrame({'Time': timestamps, "ocr_pending_file_sizes": ocr_pending_all_file_sizes})
     fig = px.line(df, x='Time', y='ocr_pending_file_sizes',
                         title='Ocr Conversion Server')
-    output_file = os.path.join(os.path.dirname(history_filename), "ocr_pending_file_sizes.html")
-    build_html(fig, output_file)
+    build_html(args, fig, "ocr_pending_file_sizes.html")
 
 
 def main(args):
     if args.central_stats_file is not None:
-        process_cumulative_stats(args.central_stats_file)
-        process_point_stats(args.central_stats_file)
+        process_cumulative_stats(args)
+        process_point_stats(args)
     if args.conversion_server_stats is not None:
-        process_convert_stats(args.conversion_server_stats)
+        process_convert_stats(args)
 
 
 if __name__ == "__main__":
