601473df1582a4e00e4ba29f9c069511165c95f5
sokirko74 <sokirko@yandex.ru>
2020-10-07 16:20:24 +0300

    add robots.txt to disclosures.ru

--- tools/disclosures/disclosures/urls.py
+++ tools/disclosures/disclosures/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('declarations.urls')),
-
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
