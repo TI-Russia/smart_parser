855c8c6e7d8192928f18b73ef114f85347b34daa
sokirko74 <sokirko@yandex.ru>
2020-10-24 05:03:09 +0300

    add git pull

--- tools/disclosures_site/declarations/management/commands/clear_dedupe_artefacts.py
+++ tools/disclosures_site/declarations/management/commands/clear_dedupe_artefacts.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
             cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
             cursor.execute("""
                delete from declarations_person 
-               where id in (select person_id from declarations_section where dedupe_score > 0
+               where id in (select person_id from declarations_section where dedupe_score > 0)
                """);
             cursor.execute("update declarations_section set person_id=null where dedupe_score > 0;")
             cursor.execute("update declarations_section set dedupe_score = 0;")
