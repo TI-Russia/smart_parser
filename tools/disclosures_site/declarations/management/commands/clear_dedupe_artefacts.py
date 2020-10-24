bd6fd4db83e1e8898dfbb19b174a54734aac37f0
sokirko74 <sokirko@yandex.ru>
2020-10-24 07:35:36 +0300

    clear human mergings under --delete-all

--- tools/disclosures_site/declarations/management/commands/clear_dedupe_artefacts.py
+++ tools/disclosures_site/declarations/management/commands/clear_dedupe_artefacts.py
@@ -1,18 +1,32 @@
 from django.core.management import BaseCommand
 from django.db import connection
-
+import sys
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+                '--delete-all',
+            dest='delete_all',
+            default=False,
+            action="store_true",
+            help='delete also human mergings'
+        )
+
+    def run_sql(self, cursor, cmd):
+        sys.stdout.write(cmd + "\n")
+        cursor.execute(cmd)
+
     def handle(self, *args, **options):
         with connection.cursor() as cursor:
-            cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
-            cursor.execute("""
-               delete from declarations_person 
-               where id in (select person_id from declarations_section where dedupe_score > 0)
-               """);
-            cursor.execute("update declarations_section set person_id=null where dedupe_score > 0;")
-            cursor.execute("update declarations_section set dedupe_score = 0;")
-            cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
+            self.run_sql(cursor, "SET FOREIGN_KEY_CHECKS = 0;")
+            if options.get('delete_all', False):
+                self.run_sql(cursor, "update declarations_section set person_id=null, dedupe_score=null;")
+                self.run_sql(cursor, "truncate table declarations_person;")
+            else:
+                self.run_sql(cursor, "delete from declarations_person where id in (select person_id from declarations_section where dedupe_score > 0)");
+                self.run_sql(cursor, "update declarations_section set person_id=null where dedupe_score > 0;")
+                self.run_sql(cursor, "update declarations_section set dedupe_score = 0;")
+            self.run_sql(cursor, "SET FOREIGN_KEY_CHECKS = 1;")
