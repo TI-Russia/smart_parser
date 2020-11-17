420d94b601ee61a77336c06c988bcf74680d1140
Alvin J. Alexander <sokirko@yandex.ru>
2020-11-17 12:39:17 +0300

    delete aux files

--- tools/DeclDocRecognizer/run_folder.sh
+++ tools/DeclDocRecognizer/run_folder.sh
@@ -1,3 +1,4 @@
 FOLDER=$1
+rm $FOLDER/*.json.verdict
 rm $FOLDER/*.verdict
 ls $FOLDER  | grep -v ".txt$" | xargs --verbose -n 1 -P 5 -I {} python3  dlrecognizer.py --keep-txt --reuse-txt --source-file $FOLDER/{}
