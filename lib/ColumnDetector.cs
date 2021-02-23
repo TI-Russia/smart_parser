828281e382d96a4fa261bbae86245a7a15892ff4
Alvin J. Alexander <sokirko@yandex.ru>
2021-02-23 07:36:26 +0300

    three lines in header

--- lib/ColumnDetector.cs
+++ lib/ColumnDetector.cs
@@ -361,9 +361,24 @@ namespace Smart.Parser.Lib
                 {
                     foreach (var underCell in underCells)
                     {
-                        underCell.TextAbove = cell.Text.NormSpaces();
-                        columnCells.Add(underCell);
-                        texts.Add(underCell.TextAbove + "^" + underCell.Text.NormSpaces());
+                        var underCells2 = FindSubcellsUnder(adapter, underCell);
+                        if ((underCells2.Count() <= 1) || (maxMergedRows != 3))
+                        {
+                            underCell.TextAbove = cell.Text.NormSpaces();
+                            columnCells.Add(underCell);
+                            texts.Add(underCell.TextAbove + "^" + underCell.Text.NormSpaces());
+                        }
+                        else
+                        {
+                            // three merged rows in the header, see pudoz_01.docx
+                            foreach (var underCell2 in underCells2)
+                            {
+                                underCell2.TextAbove = cell.Text.NormSpaces() + " " + underCell.Text.NormSpaces();
+                                columnCells.Add(underCell2);
+                                texts.Add(underCell2.TextAbove + "^" + underCell2.Text.NormSpaces());
+                            }
+                        }
+
                     }
                     headerEndRow = Math.Max(headerEndRow, underCells[0].Row + underCells[0].MergedRowsCount);
                 }
@@ -443,7 +458,7 @@ namespace Smart.Parser.Lib
             {
                 string cellText1 = adapter.GetCell(firstDataRow, 0).GetText();
                 string cellText2 = adapter.GetCell(firstDataRow, 1).GetText();
-                if (cellText1 == "1" && cellText2 == "2")
+                if (cellText1.StartsWith("1") && cellText2.StartsWith("2"))
                 {
                     firstDataRow++;
                 }
