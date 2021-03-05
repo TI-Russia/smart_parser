ab4367a417c5583b4e623058880d2fface08f689
sokirko74 <sokirko@yandex.ru>
2021-03-05 21:20:47 +0300

    zip recursive

--- tools/dl_robot/scripts/add_fns_json_to_html.sh
+++ tools/dl_robot/scripts/add_fns_json_to_html.sh
@@ -3,5 +3,5 @@ cd ~/tmp
 unzip nalog_12_15.zip
 find nalog -name '*.html' | xargs -P 4 -n 1 python3 ~/smart_parser/tools/dl_robot/scripts/add_fns_json_to_html.py
 find nalog -name '*.json' | xargs rm -rf
-zip nalog_html.zip nalog
+zip -r nalog_html.zip nalog
 python3 ~/smart_parser/tools/dlrobot_server/unzip_archive.py --archive nalog_html.zip
