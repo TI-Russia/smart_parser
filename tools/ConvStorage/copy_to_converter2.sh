3688f41188e8206e779af3006d08e46adb748683
sokirko74 <sokirko@yandex.ru>
2021-03-08 05:46:16 +0300

    windows task sheduler

---
+++ tools/ConvStorage/copy_to_converter2.sh
@@ -0,0 +1,15 @@
+BACKUP_HOST=migalka
+BACKUP_FOLDER=/mnt/sdb/Yandex.Disk/declarator/conv_db
+LOG=copy_to_server.log
+RSYNC="rsync -v -r -e \"/bin/ssh\" --size-only"
+
+echo "copy started" >> $LOG
+date >> $LOG
+
+$RSYNC /cygdrive/c/tmp/conv_db/access.log $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
+$RSYNC /cygdrive/c/tmp/conv_db/converted_file_storage.json  $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
+$RSYNC /cygdrive/c/tmp/conv_db/db_converted_files $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
+$RSYNC /cygdrive/d/declarator/conv_db/db_input_files $BACKUP_HOST:$BACKUP_FOLDER   >> $LOG
+
+date >> $LOG
+echo "all done" >> $LOG
