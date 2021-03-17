1dbe0b4af3628d014d6a040e020103f5d6f9c3a5
sokirko74 <sokirko@yandex.ru>
2021-03-17 22:26:29 +0300

    fix tax office rubric

--- tools/disclosures_site/declarations/tests/rubrics/test_rubrics.py
+++ tools/disclosures_site/declarations/tests/rubrics/test_rubrics.py
@@ -1,4 +1,4 @@
-from declarations.rubrics import check_rubric, TOfficeRubrics
+from declarations.rubrics import TOfficeProps, TOfficeRubrics
 
 
 from django.test import TestCase
@@ -6,28 +6,30 @@ from django.test import TestCase
 
 class RubricTestCase(TestCase):
 
+    def check(self, name, top_parent,  rubric, immediate_parent=None):
+        pattern = TOfficeProps(name, top_parent=top_parent, immediate_parent=immediate_parent)
+        self.assertTrue(pattern.check_rubric(rubric))
+
     def test_rubrics(self):
-        self.assertTrue(check_rubric("Бабушкинский районный суд", 1316, TOfficeRubrics.Court))
-        self.assertTrue(check_rubric("Воронеж - городской округ", 627, TOfficeRubrics.Municipality))
-        self.assertTrue(check_rubric("УФСИН Брянская область", 482, TOfficeRubrics.Gulag))
-        self.assertTrue(check_rubric("Совет Федерации", -1, TOfficeRubrics.Legislature))
-        self.assertTrue(check_rubric("Министерство здравоохранения Кузбасса", 173, TOfficeRubrics.Medicine))
-        self.assertTrue(check_rubric("Избирательная комиссия Ростовской области", 3, TOfficeRubrics.Election))
-        self.assertTrue(check_rubric("Прокуратура Удмуртской республики", 8, TOfficeRubrics.Prosecutor))
-        self.assertTrue(check_rubric("Агентство по гражданской обороне, чрезвычайным ситуациям и пожарной безопасности Красноярского края",
-                                     177, TOfficeRubrics.ExecutivePower))
-        self.assertTrue(check_rubric("Федеральная служба безопасности", 464, TOfficeRubrics.Siloviki))
-        self.assertTrue(check_rubric("Федеральная служба охраны", 494, TOfficeRubrics.Siloviki))
-        self.assertTrue(check_rubric("Министерство внутренних дел", 589, TOfficeRubrics.Siloviki))
-        self.assertTrue(check_rubric("Восточно-Сибирское линейное управление внутренних дел МВД России на транспорте", 4327,
-                                     TOfficeRubrics.Siloviki))
-        self.assertTrue(check_rubric("ФГКОУ ВО Академия управления министерства внутренних дел Российской Федерации", 870,
-                                     TOfficeRubrics.Education))
-        self.assertTrue(check_rubric("Федеральная служба войск национальной гвардии Российской Федерации", 464,
-                                     TOfficeRubrics.Siloviki))
-        self.assertTrue(check_rubric("Федеральная служба государственной статистики", 464,
-                                     TOfficeRubrics.ExecutivePower))
-        self.assertTrue(check_rubric("Министерство обороны", 589,
-                                     TOfficeRubrics.Military))
-        self.assertTrue(check_rubric("Ивановский сельсовет", -1,TOfficeRubrics.Municipality))
+        self.check("Бабушкинский районный суд", 1316, TOfficeRubrics.Court)
+        self.check("Воронеж - городской округ", 627, TOfficeRubrics.Municipality)
+        self.check("УФСИН Брянская область", 482, TOfficeRubrics.Gulag)
+        self.check("Совет Федерации", -1, TOfficeRubrics.Legislature)
+        self.check("Министерство здравоохранения Кузбасса", 173, TOfficeRubrics.Medicine)
+        self.check("Избирательная комиссия Ростовской области", 3, TOfficeRubrics.Election)
+        self.check("Прокуратура Удмуртской республики", 8, TOfficeRubrics.Prosecutor)
+        self.check("Агентство по гражданской обороне, чрезвычайным ситуациям и пожарной безопасности Красноярского края",
+                                     177, TOfficeRubrics.ExecutivePower)
+        self.check("Федеральная служба безопасности", 464, TOfficeRubrics.Siloviki)
+        self.check("Федеральная служба охраны", 494, TOfficeRubrics.Siloviki)
+        self.check("Министерство внутренних дел", 589, TOfficeRubrics.Siloviki)
+        self.check("Восточно-Сибирское линейное управление внутренних дел МВД России на транспорте", 4327,
+                                     TOfficeRubrics.Siloviki)
+        self.check("ФГКОУ ВО Академия управления министерства внутренних дел Российской Федерации", 870,
+                                     TOfficeRubrics.Education)
+        self.check("Федеральная служба войск национальной гвардии Российской Федерации", 464, TOfficeRubrics.Siloviki)
+        self.check("Федеральная служба государственной статистики", 464,TOfficeRubrics.ExecutivePower)
+        self.check("Министерство обороны", 589, TOfficeRubrics.Military)
+        self.check("Ивановский сельсовет", -1, TOfficeRubrics.Municipality)
+        self.check("Федеральная служба по налогам и сборам", -1, TOfficeRubrics.Tax, immediate_parent=470)
 
