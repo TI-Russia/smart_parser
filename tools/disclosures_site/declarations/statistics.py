4e6d77e6959a9a5fdd61c6fc40e63d1e17b9dc0b
sokirko74 <sokirko@yandex.ru>
2021-03-20 13:57:24 +0300

    restore statistics

--- tools/disclosures_site/declarations/statistics.py
+++ tools/disclosures_site/declarations/statistics.py
@@ -15,7 +15,10 @@ ALL_METRIC_NAMES = {
     'sections_person_name_income_year_declarant_income_size': 'Count distinct (person_name, income_year, declarant_income_size)',
     'sections_person_name_income_year_spouse_income_size': 'Count distinct (person_name, income_year, spouse_income_size)',
     'sections_dedupe_score_greater_0': 'Sections (dedupe_score > 0)',
-    'person_count': 'People count'
+    'person_count': 'People count',
+    'sections_count': 'All sections count',
+    'sections_count_only_dlrobot': 'Sections (only_dlrobot)',
+    'sections_count_both_found': 'Sections (both found)'
 }
 
 
@@ -42,6 +45,12 @@ class TDisclosuresStatistics:
         self.metrics['source_document_only_dlrobot_count'] = models.Source_Document.objects.filter(intersection_status=TIntersectionStatus.only_dlrobot).count()
         self.metrics['source_document_only_human_count'] = models.Source_Document.objects.filter(intersection_status=TIntersectionStatus.only_human).count()
         self.metrics['source_document_both_found_count'] = models.Source_Document.objects.filter(intersection_status=TIntersectionStatus.both_found).count()
+
+        self.metrics['sections_count'] = models.Section.objects.all().count()
+        self.metrics['sections_count_only_dlrobot'] = models.Section.objects.filter(
+            source_document__intersection_status=TIntersectionStatus.only_dlrobot).count()
+        self.metrics['sections_count_both_found'] = models.Section.objects.filter(
+            source_document__intersection_status=TIntersectionStatus.both_found).count()
 
         self.metrics['sections_person_name_income_year_declarant_income_size'] = \
             sections_person_name_income_year_declarant_income_size(models.Relative.main_declarant_code)
@@ -97,6 +106,9 @@ class TDisclosuresStatisticsHistory:
         self.check_sum_metric_increase(curr, ['source_document_only_dlrobot_count', 'source_document_both_found_count'])
         self.check_sum_metric_increase(curr, ['source_document_only_human_count', 'source_document_both_found_count'])
         self.check_sum_metric_increase(curr, ["sections_dedupe_score_greater_0"])
+        # metrics sections_count, sections_count_only_dlrobot, sections_count_both_found can decrease
+        # because we make progress in finding section copies. Metric sections_dedupe_score_greater_0 can fall also
+        # but we have not seen it.
 
     @staticmethod
     def build_current_statistics(crawl_epoch):
