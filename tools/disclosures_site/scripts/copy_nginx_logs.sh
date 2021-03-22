6f68688a2a7bb576dff685a900ef66d6e603e47f
sokirko74 <sokirko@yandex.ru>
2021-03-23 01:10:31 +0300

    copy nginx logs

--- tools/disclosures_site/scripts/copy_nginx_logs.sh
+++ tools/disclosures_site/scripts/copy_nginx_logs.sh
@@ -4,13 +4,21 @@ source $COMMON_SCRIPT
 LOG_ARCHIVE=$YANDEX_DISK_FOLDER/nginx_logs
 FRONTEND_NGINX_LOGS=/var/log/nginx
 FRONTEND_OUTPUT_FOLDER=$TOOLS/disclosures/static/dlrobot
-DATE=`date "+\%Y-\%m-\%d"`
+DATE=`date "+%Y-%m-%d"`
 
-ssh $FRONTEND sudo chmod a+r $FRONTEND_NGINX_LOGS/access.log
-scp $FRONTEND:$FRONTEND_NGINX_LOGS/access.log $LOG_ARCHIVE/access.$DATE
-gzip --force $LOG_ARCHIVE/access.$DATE
+function copy_log() {
+  local log_name=$1
+  local local_log_path=$LOG_ARCHIVE/$log_name.$DATE
+  ssh $FRONTEND sudo chmod a+r $FRONTEND_NGINX_LOGS/$log_name.log
+  scp $FRONTEND:$FRONTEND_NGINX_LOGS/$log_name.log $local_log_path
+  if [ ! -s $local_log_path ]; then
+    echo "$local_log_path is empty";
+    /home/sokirko/.local/bin/telegram-send "$local_log_path is empty, no page visitors?"
+    exit 1
+  fi
+  gzip --force $local_log_path
+}
 
-ssh $FRONTEND sudo chmod a+r $FRONTEND_NGINX_LOGS/error.log
-scp $FRONTEND:$FRONTEND_NGINX_LOGS/error.log $LOG_ARCHIVE/error.$DATE
-gzip --force $LOG_ARCHIVE/error.$DATE
+copy_log access
+copy_log error
 
