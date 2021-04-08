a033de05cb78890cb0668758e25e9a902c1e6eaf
Alvin J. Alexander <sokirko@yandex.ru>
2021-04-08 10:47:19 +0300

    add test for text measure

---
+++ test/TextMeasure.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smart.Parser.Adapters;
+using TI.Declarator.ParserCommon;
+using System.IO;
+using Smart.Parser.Lib;
+using static Algorithms.LevenshteinDistance;
+using System;
+
+namespace test
+{
+    [TestClass]
+    public class StringMeasureTest
+    {
+        
+        [TestMethod]
+        public void TextMeasureTest()
+        {
+            string s2 = "тест текст";
+            TStringMeasure.InitGraphics("Liberation Serif", 10);
+            float width = TStringMeasure.MeasureStringWidth(s2);
+            Assert.AreEqual(31, (int)width);
+        }
+
+           
+    }
+}
