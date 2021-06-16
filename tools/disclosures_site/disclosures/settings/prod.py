415fb35e34e404b2ba61cfc53de6f8e5f8b2f419
sokirko74 <sokirko@yandex.ru>
2021-06-16 07:51:26 +0300

    use html tag viewport

--- tools/disclosures_site/disclosures/settings/prod.py
+++ tools/disclosures_site/disclosures/settings/prod.py
@@ -6,7 +6,8 @@ DEBUG = False
 if socket.gethostname() == "dell-7440":
     DEBUG = True
 
-ALLOWED_HOSTS = ['disclosures.ru', '95.165.96.61', 'localhost', '192.168.100.151', '192.168.100.206', '185.237.97.32']
+ALLOWED_HOSTS = ['disclosures.ru', '95.165.96.61', 'localhost', '192.168.100.151', '192.168.100.206', '185.237.97.32',
+                 '192.168.8.103', '127.0.0.1']
 
 ELASTICSEARCH_INDEX_NAMES = {
     'section_index_name': 'declaration_sections_prod',
@@ -18,5 +19,10 @@ ELASTICSEARCH_INDEX_NAMES = {
 os.environ['DISCLOSURES_DATABASE_NAME'] = os.environ.get('DISCLOSURES_DATABASE_NAME', 'disclosures_db')
 
 os.environ['SOURCE_DOC_SERVER_ADDRESS'] = os.environ.get('SOURCE_DOC_SERVER_ADDRESS', '192.168.100.151:8090')
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 from .common import *
