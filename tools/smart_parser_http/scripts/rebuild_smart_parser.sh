51084beea117e83980d534e7cb46a4c73ba001e1
sokirko74 <sokirko@yandex.ru>
2021-06-19 05:04:13 +0300

    update server list

--- tools/smart_parser_http/scripts/rebuild_smart_parser.sh
+++ tools/smart_parser_http/scripts/rebuild_smart_parser.sh
@@ -1,5 +1,5 @@
 python3 ~/smart_parser/tools/source_doc_http/scripts/print_all_keys.py ~/declarator_hdd/Yandex.Disk/declarator/source_doc/bin_files >all_source_sha256.txt
-export HOSTS="frontend,migalka,lena,avito"
+export HOSTS="samsung,migalka,lena,avito"
 JOBS_COUNT=4
 #export SOURCE_DOC_SERVER_ADDRESS=migalka:8090
 #export SMART_PARSER_SERVER_ADDRESS=migalka:8165
