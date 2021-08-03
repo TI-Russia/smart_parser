ec86cf5d99c200f0c2cc46ee665b06ea1b968894
sokirko74 <sokirko@yandex.ru>
2021-08-03 12:21:55 +0300

    add timeout to switch_to_window

--- tools/dl_robot/tests/test_declaration_link.py
+++ tools/dl_robot/tests/test_declaration_link.py
@@ -1,167 +1,50 @@
-from common.download import TDownloadEnv
-from web_site_db.robot_step import TRobotStep, TUrlInfo
-from web_site_db.robot_project import TRobotProject
-from common.http_request import THttpRequester
-from common.logging_wrapper import close_logger, setup_logging
-
-import http.server
-from unittest import TestCase
-import time
-import os
-import urllib
-import threading
-import shutil
+from web_site_db.robot_step import TRobotStep
+from dl_robot.tests.declaration_link_for_tests import TestDeclarationLinkBase
 
 
-class THttpServerHandler(http.server.BaseHTTPRequestHandler):
-    SVED_URL_PATH = '/sved.html'
-
-    def build_headers(self, content_type="text/html; charset=utf-8"):
-        self.send_response(200)
-        self.send_header("Content-type", content_type)
-        self.end_headers()
-
-    def do_GET(self):
-        self.build_headers()
-        if self.path == self.SVED_URL_PATH:
-            with open(self.server.test_file_path, "r", encoding="utf8") as inp:
-                self.wfile.write(inp.read().encode('utf8'))
-        else:
-            self.wfile.write("some text".encode('utf8'))
-
-    def do_HEAD(self):
-        if self.path.endswith('.docx'):
-            self.build_headers("application/vnd.openxmlformats-officedocument")
-        elif self.path.endswith('.doc'):
-            self.build_headers("application/msword")
-        else:
-            self.build_headers()
-
-
-def start_server(server):
-    server.serve_forever()
-
-
-class TestHTTPServer(http.server.HTTPServer):
-    def __init__(self, port):
-        self.test_file_path = None
-        super().__init__(('127.0.0.1', int(port)), THttpServerHandler)
-
-
-class TestDeclarationLink(TestCase):
+class TestDeclarationLink(TestDeclarationLinkBase):
     web_site_port = 8195
 
-    def build_url(self, path):
-        return 'http://127.0.0.1:{}{}'.format(self.web_site_port, path)
-
-    def download_website(self, file_path):
-        self.web_server.test_file_path = os.path.join(os.path.dirname(__file__), file_path)
-        assert os.path.exists(self.web_server.test_file_path)
-        TDownloadEnv.clear_cache_folder()
-        start_url = self.build_url(THttpServerHandler.SVED_URL_PATH)
-        robot_steps = [
-            {
-                'step_name': "declarations"
-            }
-        ]
-        with TRobotProject(self.logger, self.project_path, robot_steps, "result", enable_search_engine=False
-                           ) as project:
-            project.add_web_site(self.server_address)
-            office_info = project.web_site_snapshots[0]
-            office_info.create_export_folder()
-            office_info.url_nodes[start_url] = TUrlInfo(title="", step_name=None)
-
-            step_info = TRobotStep(office_info, **robot_steps[0])
-            step_info.pages_to_process[start_url] = 0
-            step_info.processed_pages = set()
-            step_info.apply_function_to_links(TRobotStep.looks_like_a_declaration_link)
-            links = list()
-            for url in step_info.step_urls:
-                u = list(urllib.parse.urlparse(url))
-                u[1] = "dummy"
-                links.append(urllib.parse.urlunparse(u))
-            return links
-
     def setUp(self):
-        TRobotStep.check_local_address = True
-        self.server_address = '127.0.0.1:{}'.format(self.web_site_port)
-        self.web_server = TestHTTPServer(self.web_site_port)
-        self.http_server_thread = threading.Thread(target=start_server, args=(self.web_server,))
-        self.http_server_thread.start()
-        time.sleep(1)
-        self.data_folder = os.path.join(os.path.dirname(__file__), "data.declaration_link")
-        if os.path.exists(self.data_folder):
-            shutil.rmtree(self.data_folder, ignore_errors=True)
-        os.mkdir(self.data_folder)
-        os.chdir(self.data_folder)
-        THttpRequester.ENABLE = False
-        TDownloadEnv.clear_cache_folder()
-        self.logger = setup_logging(log_file_name="dlrobot.log")
-        THttpRequester.initialize(self.logger)
-        self.project_path = os.path.join(self.data_folder, "project.txt")
-        TRobotProject.create_project("http://127.0.0.1:{}".format(self.web_site_port), self.project_path)
+        super().setUp(TestDeclarationLink.web_site_port, "declaration_link")
 
     def tearDown(self):
-        self.web_server.shutdown()
-        close_logger(self.logger)
-        os.chdir(os.path.dirname(__file__))
-        if os.path.exists(self.data_folder):
-            shutil.rmtree(self.data_folder, ignore_errors=True)
-        self.http_server_thread.join(1)
-        self.web_server.server_close()
-        time.sleep(1)
-        TRobotStep.check_local_address = False
-
-    def compare_to_file(self, links, file_name):
-        self.maxDiff = None
-        with open(os.path.join(os.path.dirname(__file__), file_name)) as inp:
-            canon_links = list(l.strip() for l in inp)
-            self.assertSequenceEqual(canon_links, links)
-
-    def canonize_links(self, links, file_name):
-        with open(os.path.join(os.path.dirname(__file__), file_name), "w") as outp:
-            for l in links:
-                outp.write(l + "\n")
-
-    def test_akrvo(self):
-        links = self.download_website("web_sites/arkvo/sved.html")
-        #self.canonize_links(links, 'web_sites/arkvo/found_links')
-        self.compare_to_file(links, 'web_sites/arkvo/found_links')
+        super().tearDown()
 
     def test_page_text(self):
-        links = self.download_website("web_sites/page_text/sved.html")
+        links = self.process_one_page("web_sites/page_text/sved.html")
         #self.canonize_links(links, 'web_sites/page_text/found_links')
         self.compare_to_file(links, 'web_sites/page_text/found_links')
 
     def test_other_website(self):
         save = TRobotStep.check_local_address
         TRobotStep.check_local_address = False
-        links = self.download_website("web_sites/other_website/sved.html")
+        links = self.process_one_page("web_sites/other_website/sved.html")
         # self.canonize_links(links, 'web_sites/other_website/found_links')
         self.compare_to_file(links, 'web_sites/other_website/found_links')
         TRobotStep.check_local_address = save
 
     def test_simple_doc(self):
-        links = self.download_website("web_sites/simple_doc/sved.html")
+        links = self.process_one_page("web_sites/simple_doc/sved.html")
         # self.canonize_links(links, 'web_sites/simple_doc/found_links')
         self.compare_to_file(links, 'web_sites/simple_doc/found_links')
 
     def test_admkrsk(self):
-        links = self.download_website("web_sites/admkrsk/sved.html")
+        links = self.process_one_page("web_sites/admkrsk/sved.html")
         #self.canonize_links(links, 'web_sites/admkrsk/found_links')
         self.compare_to_file(links, 'web_sites/admkrsk/found_links')
 
     def test_rosminzdrav(self):
-        links = self.download_website("web_sites/minzdrav/6_4_2.html")
+        links = self.process_one_page("web_sites/minzdrav/6_4_2.html")
         #self.canonize_links(links, 'web_sites/minzdrav/found_links')
         self.compare_to_file(links, 'web_sites/minzdrav/found_links')
 
     def test_zsro(self):
-        links = self.download_website("web_sites/zsro/sved.html")
+        links = self.process_one_page("web_sites/zsro/sved.html")
         #self.canonize_links(links, 'web_sites/zsro/found_links')
         self.compare_to_file(links, 'web_sites/zsro/found_links')
 
     def test_khabkrai(self):
-        links = self.download_website("web_sites/khabkrai/sved.html")
+        links = self.process_one_page("web_sites/khabkrai/sved.html")
         #self.canonize_links(links, 'web_sites/khabkrai/found_links')
         self.compare_to_file(links, 'web_sites/khabkrai/found_links')
