0062bcf41155a747c83860d7e41af5e46f41f0f6
sokirko74 <sokirko@yandex.ru>
2021-08-15 20:58:44 +0300

    scp wrapper

--- tools/disclosures_site/scripts/scp_wrapper.py
+++ tools/disclosures_site/scripts/scp_wrapper.py
@@ -7,7 +7,7 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--action", dest='action', help="to_remote or from_remote")
-    parser.add_argument("--file", dest='basename_file', required=True)
+    parser.add_argument("--file", dest='basename_files', required=True, nargs='+')
     parser.add_argument("--remote-host", dest='remote_host', default="195.70.213.239")
     parser.add_argument("--username", dest='username', default="sokirko")
     parser.add_argument("--remote-host-folder", dest='remote_host_folder',
@@ -22,8 +22,12 @@ class TCopier:
         self.args = args
         assert (os.environ.get("password_remote_host") is not None)
         self.remote_password = os.environ["password_remote_host"]
-        self.local_file = os.path.join(self.args.local_folder, self.args.basename_file)
-        self.remote_file = os.path.join(self.args.remote_host_folder, self.args.basename_file)
+
+    def get_files(self):
+        for l in self.args.basename_files:
+            local_file = os.path.join(self.args.local_folder, l)
+            remote_file = os.path.join(self.args.remote_host_folder, l)
+            yield local_file, remote_file
 
     def create_ssh(self):
         client = paramiko.SSHClient()
@@ -32,21 +36,25 @@ class TCopier:
         return client
 
     def get_file_from_remote_host(self):
-        if os.path.exists(self.local_file):
-            print("rm " + self.local_file)
-            os.unlink(self.local_file)
-            
         ssh = self.create_ssh()
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.get(remote_path=self.remote_file, local_path=self.local_file)
-        print("read file  {} from remote host".format(self.local_file))
+
+        for local_file, remote_file in self.get_files():
+            if os.path.exists(local_file):
+                print("rm " + local_file)
+                os.unlink(local_file)
+
+            with SCPClient(ssh.get_transport()) as scp:
+                scp.get(remote_path=remote_file, local_path=local_file)
+            print("read file  {} from remote host".format(local_file))
 
     def put_file_to_remote_host(self):
         ssh = self.create_ssh()
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.put(self.local_file, remote_path=self.remote_file)
 
-        print("save {} to remote host".format(self.local_file))
+        for local_file, remote_file in self.get_files():
+            with SCPClient(ssh.get_transport()) as scp:
+                scp.put(local_file, remote_path=remote_file)
+
+            print("save {} to remote host".format(local_file))
 
 
 if __name__ == '__main__':
