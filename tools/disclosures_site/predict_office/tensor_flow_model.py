77c33c3850c7af2c411da7ad3563946036b948b9
sokirko74 <sokirko@yandex.ru>
2021-08-16 20:31:53 +0300

    delete unused code

--- tools/disclosures_site/predict_office/tensor_flow_model.py
+++ tools/disclosures_site/predict_office/tensor_flow_model.py
@@ -1,11 +1,10 @@
-import sys
-
 from disclosures_site.predict_office.prediction_case import TPredictionCase
 from disclosures_site.predict_office.base_ml_model import TPredictionModelBase
 from disclosures_site.predict_office.office_index import TOfficePredictIndex
 import operator
 import numpy as np
 import tensorflow as tf
+
 
 #https://github.com/tensorflow/tensorflow/issues/23748
 # https://medium.com/dailymotion/how-to-design-deep-learning-models-with-sparse-inputs-in-tensorflow-keras-fd5e754abec1
@@ -41,22 +40,6 @@ class DenseLayerForSparse(tf.keras.layers.Layer):
 
 class TTensorFlowOfficeModel(TPredictionModelBase):
 
-    def get_web_domain_feature(self, case: TPredictionCase):
-        web_domain_index = self.office_index.get_web_domain_index(case.web_domain)
-        shape = [1, len(self.office_index.web_domains)]
-        return tf.SparseTensor(indices=[(0, web_domain_index)], values=[1], dense_shape=shape)
-
-    def convert_to_sparse_tensor(self, indices_to_be_set, shape):
-        if len(indices_to_be_set) == 0:
-            return tf.SparseTensor(indices=[[0, 0]],
-                                   values=[0],
-                                   dense_shape=shape)
-        else:
-            indices_to_be_set = list((0, i) for i in sorted(list(indices_to_be_set)))
-            return tf.SparseTensor(indices=indices_to_be_set,
-                           values=[1]*len(indices_to_be_set),
-                           dense_shape=shape)
-
     def get_bigram_feature(self, case: TPredictionCase):
         site_title = self.office_index.web_sites.get_title_by_web_domain(case.web_domain)
         bigrams = set()
@@ -65,8 +48,6 @@ class TTensorFlowOfficeModel(TPredictionModelBase):
             if bigram_id is not None:
                 bigrams.add(bigram_id)
         return sorted(list(bigrams))
-        #shape = [self.office_index.get_bigrams_count(), 1]
-        #return self.convert_to_sparse_tensor(bigrams, shape)
 
     def to_ml_input_features(self, cases, verbose=False):
         def get_bigram_feature_gen():
@@ -92,8 +73,6 @@ class TTensorFlowOfficeModel(TPredictionModelBase):
                                   values=values,
                                   dense_shape=(len(cases), len(self.office_index.web_domains))
                                   )
-
-
         return {
             "bigrams_feat": bigrams,
             "web_domain_feat": web_domains,
@@ -106,22 +85,18 @@ class TTensorFlowOfficeModel(TPredictionModelBase):
 
     def init_model_before_train(self, dense_layer_size, input_data):
         inputs = list()
-        # concate_len = 0
-        # for i in input_data:
-        #     inputs.append(tf.keras.Input(name=i, tensor=input_data[i]))
-        #     concate_len += input_data[i].shape[1]
-        # print(inputs[0])
-        bigr_count = self.office_index.get_bigrams_count()
-        dom_count = len(self.office_index.web_domains)
-        inputs = [
-           tf.keras.Input(shape=(bigr_count), name="bigrams_feat", sparse=True),
-           tf.keras.Input(shape=(dom_count), name="web_domain_feat", sparse=True)
-        ]
-        concate_len = bigr_count + dom_count
+        len_sum = 0
+        for i in input_data:
+            len = input_data[i].shape[1]
+            inputs.append(tf.keras.Input(name=i, shape=(len,), sparse=True))
+            len_sum += len
+
         concatenated_layer = tf.keras.layers.concatenate(inputs)
+
+        #ValueError: The last dimension of the inputs to `Dense` should be defined. Found `None`
         #dense_layer1 = tf.keras.layers.Dense(dense_layer_size)(concatenated_layer)
-        #dense_layer1 = tf.keras.layers.Dense(dense_layer_size)(inputs)
-        dense_layer1 = DenseLayerForSparse(concate_len, dense_layer_size)(concatenated_layer)
+
+        dense_layer1 = DenseLayerForSparse(len_sum, dense_layer_size)(concatenated_layer)
 
 
         dense_layer2 = tf.keras.layers.Dense(dense_layer_size)(dense_layer1)
@@ -160,12 +135,9 @@ class TTensorFlowOfficeModel(TPredictionModelBase):
                       workers=workers_count,
                       batch_size=batch_size,
                       steps_per_epoch=steps_per_epoch,
-                      #validation_split=0.2  not supported by sparse tensors
                       )
         self.logger.info("save to {}".format(self.model_path))
         model.save(self.model_path)
-        #model = tf.keras.models.load_model(self.model_path)
-
 
     def load_model(self):
         self.logger.info("load tensorflow model from {}".format(self.model_path))
