4da575456bccc542a6db7d999b50d3839dee7a32
sokirko74 <sokirko@yandex.ru>
2021-10-16 16:02:05 +0300

    udpate script

--- tools/ConvStorage/start_server.sh
+++ tools/ConvStorage/start_server.sh
@@ -4,10 +4,14 @@
 
 cd `dirname $0`
 export DECLARATOR_CONV_URL=192.168.100.152:8091
-export PYTHONPATH=C:/tmp/smart_parser/smart_parser/tools
+export TOOLS=c:/tmp/smart_parser/smart_parser/tools
+export PYTHONPATH=$TOOLS
+export PYTHON=C:/Users/sokir/AppData/Local/Microsoft/WindowsApps/python.exe
+
+
 while True; do
-  nice --20 python C:/tmp/smart_parser/smart_parser/tools/ConvStorage/conv_storage_server.py --server-address $DECLARATOR_CONV_URL --db-json converted_file_storage.json \
-		--ocr-input-folder C:/tmp/conv_db/pdf.ocr --ocr-output-folder  C:/tmp/conv_db/pdf.ocr.out
+  nice --20 $PYTHON $TOOLS/ConvStorage/conv_storage_server.py --server-address $DECLARATOR_CONV_URL --db-json converted_file_storage.json \
+		--ocr-input-folder c:/tmp/conv_db/pdf.ocr --ocr-output-folder  c:/tmp/conv_db/pdf.ocr.out
 
   date
   echo "restart conv_storage_server.py"
