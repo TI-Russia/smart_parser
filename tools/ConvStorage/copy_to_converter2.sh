a1a947e66b20aa060d3385db89eabd3d4246b3e5
sokirko74 <sokirko@yandex.ru>
2021-12-23 14:20:08 +0300

    do not copy source files

--- tools/ConvStorage/copy_to_converter2.sh
+++ tools/ConvStorage/copy_to_converter2.sh
@@ -9,7 +9,9 @@ date >> $LOG
 $RSYNC /cygdrive/c/tmp/conv_db/access.log $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
 $RSYNC /cygdrive/c/tmp/conv_db/converted_file_storage.json  $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
 $RSYNC /cygdrive/c/tmp/conv_db/db_converted_files $BACKUP_HOST:$BACKUP_FOLDER  >> $LOG
-$RSYNC /cygdrive/d/declarator/conv_db/db_input_files $BACKUP_HOST:$BACKUP_FOLDER   >> $LOG
+
+#too much space (more than 500 GB)
+#$RSYNC /cygdrive/d/declarator/conv_db/db_input_files $BACKUP_HOST:$BACKUP_FOLDER   >> $LOG
 
 date >> $LOG
 echo "all done" >> $LOG
