dd524dea693f1f58df38d4174e3220ea7b29ebe8
sokirko74 <sokirko@yandex.ru>
2022-01-27 10:57:25 +0300

    use rosstat data for income compare

--- tools/disclosures_site/declarations/nominal_income.py
+++ tools/disclosures_site/declarations/nominal_income.py
@@ -9,38 +9,12 @@ class YearIncome:
     def __str__(self):
         return "YearIncome({},{})".format(self.year, self.income)
 
-#average by the following sources:
-# https://ac.gov.ru/archive/files/publication/a/20967.pdf
-# https://ac.gov.ru/uploads/2-Publications/rus_feb_2020.pdf
-# https://ac.gov.ru/files/publication/a/11944.pdf
-
-
-# must be without gaps
-RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY = [
-    YearIncome(2010, 0.11),
-    YearIncome(2011, 0.11),
-    YearIncome(2012, 0.11),
-    YearIncome(2013, 0.117),
-    YearIncome(2014, 0.069),
-    YearIncome(2015, 0.105),
-    YearIncome(2016, 0.0151),
-    YearIncome(2017, 0.025),
-    YearIncome(2018, 0.041),
-    YearIncome(2019, 0.061),
-]
-
-def find_year(year):
-    for i in range(len(RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY)):
-        if RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY[i].year == year:
-            return i
-    return -1
-
 #таблица ROSSTAT_ALL_RUSSIA_AVERAGE_MONTH_INCOME взята из https://rosstat.gov.ru/folder/210/document/13396
 #(Социально-экономические показатели Российской Федерации в 1991-2020 гг.) - это архив, из него надо взять файл "Ретро_2021_Раздел5"
 # в этой таблице читаем строку "Среднедушевые денежные доходы населения (в месяц), руб.
 # Объяснение, что такое "Среднедушевые денежные доходы" можно найти здесь
 # https://www.gks.ru/bgd/regl/b12_14p/IssWWW.exe/Stg/d01/05-met.htm
-
+# in RUR
 ROSSTAT_ALL_RUSSIA_AVERAGE_MONTH_INCOME = {
     2000: 2281,
     2001: 3062,
@@ -96,39 +70,29 @@ MROT = {
 IncomeCompare = namedtuple('IncomeCompare', ['population_income', 'declarant_income', 'min_year', 'max_year'])
 
 
+def get_income_diff(income1, income2):
+    return int(100.0 * (float(income2) - float(income1)) / float(income1))
+
 #years are not continous but ordered by year
 def get_average_nominal_incomes(year_incomes):
     if len(year_incomes) <= 1:
         return None
-    debug = ",".join(str(i) for i in year_incomes)
-    first_declarant_income = None
-    population_start_index = None
-    last_declarant_income = None
-    population_end_index = None
+    first_income = None
+    last_income = None
     for year_income in year_incomes:
         if year_income.income == 0 or year_income.income is None:
             continue
         if year_income.year not in MROT:
             continue
-
         if year_income.income < 12*MROT[year_income.year]:
             continue
-
-        k = find_year(year_income.year)
-        if k != -1:
-            if first_declarant_income is None:
-                population_start_index = k + 1
-                first_declarant_income = year_income.income
-            last_declarant_income = year_income.income
-            population_end_index = k + 1
-    if population_start_index is None or population_start_index == population_end_index:
+        if year_income.year in ROSSTAT_ALL_RUSSIA_AVERAGE_MONTH_INCOME:
+            if first_income is None:
+                first_income = year_income
+            last_income = year_income
+    if first_income is None or first_income == last_income:
         return None
-    growth = 1.0
-    for i in range(population_start_index, population_end_index):
-        growth = growth + growth * RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY[i].income
-    declarant_growth = (float(last_declarant_income) - float(first_declarant_income)) / float(first_declarant_income)
-    population_growth = growth - 1.0
-    return IncomeCompare(int(100.0 * population_growth), \
-           int(100.0 * declarant_growth), \
-           RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY[population_start_index - 1].year, \
-           RUSSIAN_NOMINAL_INCOME_GROWTHS_YEARLY[population_end_index - 1].year)
+    declarant_growth = get_income_diff(first_income.income, last_income.income)
+    population_growth = get_income_diff(ROSSTAT_ALL_RUSSIA_AVERAGE_MONTH_INCOME[first_income.year],
+                                        ROSSTAT_ALL_RUSSIA_AVERAGE_MONTH_INCOME[last_income.year])
+    return IncomeCompare(population_growth, declarant_growth, first_income.year, last_income.year)
