58f1c302e4873b6b23c1808728113ccf78292508
sokirko74 <sokirko@yandex.ru>
2022-02-08 06:23:49 +0300

    add new dlrobot case for sudrf.

--- tools/office_db/year_income.py
+++ tools/office_db/year_income.py
@@ -1,11 +1,18 @@
 class TYearIncome:
-    def __init__(self, year, income):
+    def __init__(self, year: int, income: int):
+        assert 2000 < year < 2030
         self.year = year
         self.income = income
 
     def __str__(self):
         return "TYearIncome({},{})".format(self.year, self.income)
 
+    def __lt__(self, other):
+        if self.year != other.year:
+            return self.year < other.year
+        return self.income < other.income
+
+    # Russian: темп прироста
     @staticmethod
-    def get_income_diff(income1, income2):
+    def get_growth_rate(income1, income2):
         return int(100.0 * (float(income2) - float(income1)) / float(income1))
