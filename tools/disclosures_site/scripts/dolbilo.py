3afe7e2d35d089b4b86d8dbbafef83f59cf327c4
sokirko74 <sokirko@yandex.ru>
2022-02-18 10:19:31 +0300

    store pure requests for dolbilo

--- tools/disclosures_site/scripts/dolbilo.py
+++ tools/disclosures_site/scripts/dolbilo.py
@@ -1,5 +1,4 @@
 from common.logging_wrapper import setup_logging
-from common.access_log import get_human_requests
 import argparse
 import sys
 import http.client
@@ -11,20 +10,24 @@ def parse_args(arg_list):
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", dest='host')
     parser.add_argument("--use-http", dest='use_http', action="store_true", default=False)
-    parser.add_argument("--input-access-log", dest='input_access_log')
-    parser.add_argument("--expected-normal-count", dest='expected_normal_count', type=int)
+    parser.add_argument("--input-requests", dest='input_requests')
     parser.add_argument("--user-agent", dest='user_agent', required=False, default="Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)")
     return parser.parse_args(arg_list)
+
+
+def read_requests(input_requests_path):
+    with open(input_requests_path) as inp:
+        for l in inp:
+            yield l.strip()
 
 
 if __name__ == "__main__":
     logger = setup_logging(log_file_name="dolbilo.log")
     args = parse_args(sys.argv[1:])
-    requests = get_human_requests(args.input_access_log)
     start_time = time.time()
     request_count = 0
     normal_response_count = 0
-    for request in requests:
+    for request in read_requests(args.input_requests):
         if args.use_http:
             conn = http.client.HTTPConnection(args.host)
         else:
@@ -46,23 +49,17 @@ if __name__ == "__main__":
             res = conn.getresponse()
         if res.status == 200:
             data = res.read()
-            normal_response_count += 1
+        else:
+            raise Exception("cannot fetch url={}".format(request))
         req_end = time.time()
         req_time = int(1000.0*(req_end - req_start))
         logger.debug("{}\t{}\t{}\t{}".format(request, res.status, len(data), req_time))
-        sys.stdout.flush()
         request_count += 1
 
     end_time = time.time()
     rps = round(float(request_count) / (end_time - start_time), 2)
     metrics = {
-        'normal_response_count': normal_response_count,
         'rps': rps,
         'request_count': request_count
     }
-    logger.debug(json.dumps(metrics))
-    print(json.dumps(metrics))
-    if args.expected_normal_count is not None:
-        if args.expected_normal_count != normal_response_count:
-            logger.error("expected_normal_count = {}, read_count={}".format(args.expected_normal_count, normal_response_count))
-            sys.exit(1)
+    logger.info(json.dumps(metrics))
